cc4716e0480f78738f0a4680fdb8283c4ba2f4eb
Siddharth shukla <129192558+godslayer1515@users.noreply.github.com>
2025-04-22 12:45:38 +0530

    modified:   server.js

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ io.on("connection", (socket) => {
     }
   });
 });
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () =>
   console.log(`Your future runs on the Port :- ${PORT}`)
